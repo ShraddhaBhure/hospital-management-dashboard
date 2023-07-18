bc37a1ed03339b10570900702677d698780c852a
Shraddha Bhure <122206885+ShraddhaBhure@users.noreply.github.com>
2023-07-18 16:11:34 +0530

    USERCREATE

--- HosDashboard/Controllers/LoginController.cs
+++ HosDashboard/Controllers/LoginController.cs
@@ -2,75 +2,181 @@
 using HS.Models;
 using Microsoft.AspNetCore.Identity;
 using HS.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HosDashboard.Controllers
 {
     public class LoginController : Controller
     {
 
-        private readonly MainContext _db;
+        private readonly MainContext _dbContext;
 
         public LoginController(MainContext db)
         {
-            _db = db;
+            _dbContext = db;
         }
 
-        [HttpGet]
-        public IActionResult LoginPage()
-        {
-            return View();
-        }
+
 
         [HttpGet]
         public IActionResult LoginPage2()
         {
             return View();
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> LoginPage(UserLogin model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = _db.UserLogin.FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password);
-
-                if (user != null)
-                {
-                    // Redirect to the desired page after successful login
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            return View(model);
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LoginPage2(UserLogin model)
         {
-            if (ModelState.IsValid)
+         
+                var user = await _dbContext.UserLogin.FirstOrDefaultAsync(u => u.Username == model.Username && u.Password == model.Password);
+           
+            if (user == null)
             {
-                var user = _db.UserLogin.FirstOrDefault(u => u.Username == model.Username && u.Password == model.Password);
-
-                if (user != null)
-                {
-                    // Redirect to the desired page after successful login
-                    return RedirectToAction("Index", "Home");
-                }
+                ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                return View(model);
             }
 
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            return View(model);
+            return RedirectToAction("Index", "Home");
         }
-        [HttpPost]
+    
+     
         public IActionResult Logout()
         {
             // Perform the necessary logout actions, such as clearing authentication cookies or session variables
             // Example:
             // HttpContext.Session.Clear();
 
-            return RedirectToAction("Login");
+            return RedirectToAction("LoginPage2");
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            var users = await _dbContext.UserLogin.ToListAsync();
+            return View(users);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserLogin user)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UserLogin.Add(user);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+
+    
+            public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUser(UserLogin user)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UserLogin.Add(user);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _dbContext.UserLogin.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, UserLogin user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dbContext.UserLogin.Update(user);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _dbContext.UserLogin.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _dbContext.UserLogin.FindAsync(id);
+            _dbContext.UserLogin.Remove(user);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _dbContext.UserLogin.Any(e => e.Id == id);
         }
     }
 
--- HosDashboard/Program.cs
+++ HosDashboard/Program.cs
@@ -36,7 +36,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=LoginPage2}/{id?}");
-  //pattern: "{controller=Home}/{action=Index}/{id?}");
+   pattern: "{controller=Login}/{action=LoginPage2}/{id?}");
+ //pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
